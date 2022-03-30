094c37da94ea32f885e73c8365a04fc64e634d82
Daniel Nelson <dan.s.nelson@gmail.com>
2022-03-30 19:26:09 -0400

    connected to OMDb API, created color functionality to represent release date on chart, modeled chart on website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,29 @@
-var queryURL = "https://api.covidtracking.com/v2/states/ca/daily.json"
-//make sure to use v.2. The above gets data for California. Will need to convert state into 2-letter state code
+var queryURL = "https://api.covidtracking.com/v2/us/daily.json"
+// make sure to use v.2. The above gets data for California. Will need to convert state into 2-letter state code
 dataList = [];
 dateList = [];
-
+colorList = [];
 fetch(queryURL)
     .then(function (response) {
         return response.json();
     })
     .then(function(data){
         console.log(data);
-        for (i=(data.data.length-8); i>=0; i--) {
-            console.log(dataList);
-            console.log("hi");
-            newDataList = dataList.push(data.data[i].cases.total.calculated.change_from_prior_day);
-            console.log(newDataList);
+        for (i=(data.data.length-55); i>=0; i--) {
+            newDataList = dataList.push(data.data[i].cases.total.calculated.seven_day_change_percent);
             newDateList = dateList.push(data.data[i].date)
-            console.log(dateList);
+        }
+        for (i=0; i<366; i++) {
+            newColorList = colorList.push('rgba(54, 162, 235, 0.2)');
         }
 
-        // logList = logConvert(dataList);
-        // console.log(logList);
         console.log(dateList);
+        console.log((colorList));
+        logList = logConvert(dataList);
+
+        colorData = colorDate('2020-12-08', dateList, colorList);
+        console.log((colorData));
+
 
         const ctx = document.getElementById('myChart');
         const myChart = new Chart(ctx, {
@@ -29,8 +32,8 @@ fetch(queryURL)
                 labels: dateList,
                 datasets: [{
                     label: 'Dates',
-                    // data: logList,
-                    data: dataList,
+                    data: logList,
+                    backgroundColor: colorData,
                 }]
             },
             options: {
@@ -53,5 +56,36 @@ function logConvert(dataList) {
     return logList;
 }
 
+function colorDate(date, dateList, colorList) {
+    for (i=0; i<dateList.length; i++) {
+        if (date === dateList[i]) {
+            console.log("hi!");
+            colorList.splice(i,0,'rgba(255, 10, 32, 3.8)')
+            colorList.splice(i+1,1);
+        }
+    }
+    return colorList;
+}
+
+// function dayToDayPercentChange(dataList) {
+//     newList = [];
+//     for (i=0; i<dataList.length; i++) {
+//         newList.push(dataList[i]/dataList[i-1])
+//     }
+//     return newList;
+// }
 
 
+//For dynamically resizing charts once they are nested in page
+//https://www.chartjs.org/docs/3.2.1/configuration/responsive.html
+
+
+// var newQueryURL = "http://www.omdbapi.com/?y=2020&apikey=c3d2c14c"
+
+// fetch(newQueryURL)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function(data){
+//         console.log(data);
+//     })
